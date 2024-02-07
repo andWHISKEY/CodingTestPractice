8ba8e8ead5c94e66deb40da364b769b3b044b049
andWHISKEY <86095630+andWHISKEY@users.noreply.github.com>
2024-02-07 21:43:08 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -3,7 +3,6 @@ def solution(clothes):
     # temp=list(map(list,zip(*clothes)))
     # dic=Counter(temp[1])
     dic=Counter([types for name, types in clothes])
-    # print(dic)
     sum=1
     for i in list(dic.values()):
         sum*=(i+1)
