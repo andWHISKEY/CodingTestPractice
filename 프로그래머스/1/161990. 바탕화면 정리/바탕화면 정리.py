f8f2031e5bc4f07c6f36620ed881465d63ef850e
andWHISKEY <86095630+andWHISKEY@users.noreply.github.com>
2024-03-06 21:49:57 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.74 ms, Memory: 28.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -16,11 +16,4 @@ def solution(wallpaper):
     # print(wallpaper)
     loc = np.where(wallpaper==1)
     # print(loc)
-    return [int(min(loc[0])),int(min(loc[1])),int(max(loc[0])+1),int(max(loc[1])+1)]
-    # a,b=list(map(list,*wallpaper))
-    # print(a)
-    # print(b)
-    
-    # print(wallpaper)
-    # answer = []
-    # return answer
+    return int(min(loc[0])),int(min(loc[1])),int(max(loc[0])+1),int(max(loc[1])+1)
