bf6ea9c6d8f80f2af270fcc2822fea6048d088e4
andWHISKEY <86095630+andWHISKEY@users.noreply.github.com>
2024-03-12 21:56:13 +0900

    Delete 610-triangle-judgement directory

--- 610-triangle-judgement/triangle-judgement.sql
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-SELECT x,y,z,case when greatest(x,y,z)=x and y+z>x then 'Yes' when greatest(x,y,z)=y and x+z>y then 'Yes' when greatest(x,y,z)=z and x+y>z then 'Yes' Else 'No' End AS triangle
-FROM Triangle
-
-
