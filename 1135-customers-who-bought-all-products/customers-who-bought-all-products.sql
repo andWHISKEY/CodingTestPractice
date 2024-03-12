d8ee5960e193cfcc8ab505cc003138811a4f1255
andWHISKEY <86095630+andWHISKEY@users.noreply.github.com>
2024-03-12 21:55:27 +0900

    Delete 1135-customers-who-bought-all-products directory

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT C.customer_id
-FROM Customer C, Product P
-WHERE C.product_key in (P.product_key)
-GROUP BY C.customer_id
-HAVING COUNT(Distinct C.product_key)=(SELECT COUNT(*) From Product) 
