a33f9cbfcaff979dd93c9480f6c81680772eeaa5
andWHISKEY <86095630+andWHISKEY@users.noreply.github.com>
2024-03-12 15:33:20 +0900

    [level 2] Title: 삼각 달팽이, Time: 4581.81 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -3,20 +3,24 @@ def triangle(n,num,x,y,arr):
     if n<0:
         return arr
     elif n-1==0:
+        # print('마지막 i:X','n:',n,x,y,'num:',num)
         arr[x][y]=num
         return arr
     #하향
     for i in range(n-1):
+        # print('하향   i:',i,'n:',n,x,y,'num:',num)
         arr[x][y]=num
         x+=1
         num+=1
     #우향
     for i in range(n-1):
+        # print('우향   i:',i,'n:',n,x,y,'num:',num)
         arr[x][y]=num
         y+=1
         num+=1    
     #대각선
     for i in range(n-1):
+        # print('대각선 i:',i,'n:',n,x,y,'num:',num)
         arr[x][y]=num
         x-=1
         y-=1
@@ -28,10 +32,9 @@ def triangle(n,num,x,y,arr):
     
     
 def solution(n):
-    N=n
     result=[]
     arr=[[0]*n for i in range(n)]
-    triangle(N,1,0,0,arr)
+    triangle(n,1,0,0,arr)
     arr=sum(arr,[])
     for x in arr:
         if x>0:
