ea2b2bdf6ff60248b33ce19f707facb83e5a16c5
andWHISKEY <86095630+andWHISKEY@users.noreply.github.com>
2024-04-26 00:55:50 +0900

    [level 3] Title: 조건별로 분류하여 주문상태 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -1,10 +1,4 @@
-# 난 column 추가해서 풀려했는데, 여기선 그렇게 푸는게 아니라 CASE IF ELSE로 풀어야함. 아래는 에러 
-# ALTER TABLE FOOD_ORDER ADD COLUMN 출고여부 varchar(100)
-# OUT_DATE그대로 출력시 몇시몇분몇초가 나옴, 이때 DATE_FORMAT(OUT_DATE,'%Y-%m-%d')처리 필요
-SELECT ORDER_ID,PRODUCT_ID,DATE_FORMAT(OUT_DATE,'%Y-%m-%d') AS OUT_DATE,
-CASE WHEN OUT_DATE <= '2022-05-01' THEN '출고완료'
-WHEN OUT_DATE > '2022-05-01' THEN '출고대기'
-ELSE '출고미정' 
-END AS '출고여부'
+-- 코드를 입력하세요
+SELECT ORDER_ID,PRODUCT_ID,DATE_FORMAT(OUT_DATE,'%Y-%m-%d'),CASE WHEN out_date<='2022-05-01' then '출고완료' WHEN out_date is null then '출고미정' else '출고대기' end as 출고여부  
 FROM FOOD_ORDER
-ORDER BY ORDER_ID ASC
+ORDER BY ORDER_ID asc
