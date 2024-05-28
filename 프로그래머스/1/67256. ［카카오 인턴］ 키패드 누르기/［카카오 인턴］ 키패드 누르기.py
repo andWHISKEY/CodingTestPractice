8a2ca35086ee78aaf0c00f78d50fb400e0b6e59f
whi$key <86095630+andWHISKEY@users.noreply.github.com>
2024-05-29 00:50:38 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.34 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,41 +1,37 @@
 def solution(numbers, hand):
-    result = ''
-    dic = {1: [0, 0], 2: [0, 1], 3: [0, 2],
-           4: [1, 0], 5: [1, 1], 6: [1, 2],
-           7: [2, 0], 8: [2, 1], 9: [2, 2],
-           '*':[3, 0], 0: [3, 1], '#': [3, 2]}
-    left_loc=dic['*']
-    right_loc=dic['#']
+
+    keypad = {  1:(0,1), 2:(0,2), 3:(0,3),
+                4:(1,1), 5:(1,2), 6:(1,3),
+                7:(2,1), 8:(2,2), 9:(2,3),
+              '*':(3,1), 0:(3,2), '#':(3,3) }
     
-    for i in numbers:
-        if i in [1,4,7]:
+    l_pos=keypad['*']
+    r_pos=keypad['#']
+    
+    result=''
+    
+    for num in numbers:
+        if num in [1,4,7]:
             result+='L'
-            left_loc=dic[i]
-        elif i in [3,6,9]:
+            l_pos=keypad[num]
+        elif num in [3,6,9]:
             result+='R'
-            right_loc=dic[i]
-        #2,5,8,0
+            r_pos=keypad[num]
         else:
-            left_dis=0
-            right_dis=0
-            
-            #얼마나 가까운지 어떻게 계산하지? 
-            for a,b,c in zip(left_loc,right_loc,dic[i]):
-                left_dis+=abs(a-c)
-                right_dis+=abs(b-c)
-            
-            if left_dis<right_dis:
+            l_distance= abs(keypad[num][0]-l_pos[0]) + abs(keypad[num][1]-l_pos[1])
+            r_distance= abs(keypad[num][0]-r_pos[0]) + abs(keypad[num][1]-r_pos[1])
+            if l_distance<r_distance:
                 result+='L'
-                left_loc=dic[i]
-            elif left_dis>right_dis:
+                l_pos=keypad[num]
+            elif l_distance>r_distance:
                 result+='R'
-                right_loc=dic[i]
-            else:
+                r_pos=keypad[num]
+            elif l_distance==r_distance:
                 if hand=='left':
                     result+='L'
-                    left_loc=dic[i]
+                    l_pos=keypad[num]
                 else:
                     result+='R'
-                    right_loc=dic[i]
-    
+                    r_pos=keypad[num]
+
     return result
