ae60c3665965b5c98f629c62b069b6fac385cb38
whi$key <86095630+andWHISKEY@users.noreply.github.com>
2024-12-16 08:20:55 +0900

    [level 3] Title: 가장 먼 노드, Time: 56.89 ms, Memory: 23.2 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=python3) 
 
 ### 성능 요약
 
-메모리: 20.1 MB, 시간: 72.27 ms
+메모리: 23.2 MB, 시간: 56.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 2일 10:50:33
+2024년 12월 16일 08:20:51
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,56 +1,23 @@
-from collections import deque
-
-def bfs(graph,start,visited=[]):
-    queue=deque([start])
-    visited[start]=True
-    answer=0
-    while queue:
-        node=queue.popleft()
-        for i in graph[node]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i]=visited[node]+1
-    # 가장 멀리 떨어진 노드 수 계산
-    max_edge=max(visited)
-    for i in visited:
-        if i ==max_edge:
-            answer+=1
-    return answer
-
 def solution(n, edge):
-    graph=[[] for _ in range(n+1)]
-    visited=[False]*(n+1)  
-    edge.sort()
-    for x,y in edge:
-        graph[x].append(y)
-        graph[y].append(x)
-    # print(graph)
-    return bfs(graph,1,visited)
-    
-    
-    
+    graph =[  [] for _ in range(n + 1) ]
+    distances = [ 0 for _ in range(n) ]
+    is_visit = [False for _ in range(n)]
+    queue = [0]
+    is_visit[0] = True
+    for (a, b) in edge:
+        graph[a-1].append(b-1)
+        graph[b-1].append(a-1)
 
+    while queue:
+        i = queue.pop(0)
 
+        for j in graph[i]:
+            if is_visit[j] == False:
+                is_visit[j] = True
+                queue.append(j)
+                distances[j] = distances[i] + 1
 
+    distances.sort(reverse=True)
+    answer = distances.count(distances[0])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return answer
