223a044f837c23d256ce9e3b346f8e09464ef8cd
whi$key <86095630+andWHISKEY@users.noreply.github.com>
2024-12-20 23:58:06 +0900

    [level 2] Title: 가장 큰 수, Time: 69.39 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,12 @@
 def solution(numbers):
-    answer = ''
-    # numbers=str(numbers)
-    for idx,i in enumerate(numbers):
-        numbers[idx]=str(i)
-    numbers.sort(key = lambda x : x*3,reverse=True)
+    
+    numbers = sorted([str(n) for n in numbers], key = lambda x : x*3, reverse = True)
     # print(numbers)
-    return (str(int(('').join(numbers))))
+    answer=''.join(numbers)
+    while len(answer)>=1 and answer[0]=='0':
+        answer=answer[1:]
+    
+    if len(answer)==0:
+        return "0"    
+    
+    return answer
